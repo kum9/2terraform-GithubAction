7d0c524b41fe0a9689eb6eb4c53be4c4555ac092
wasim <wasim.hannure@yahoo.in>
2024-09-08 15:28:26 +0530

    github action

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -2,12 +2,16 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.66.0"
+      version = "5.10.0"
     }
   }
 }
+
 
 provider "aws" {
   # Configuration options
   region = "ap-south-1"
 }
+
+
+
