455ff02bbaaa142ec2a8b064deef361302a15c1d
wasim <wasim.hannure@yahoo.in>
2024-09-08 15:17:08 +0530

    github action

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.16.1"
+      source = "hashicorp/aws"
+      version = "5.66.0"
     }
+  }
 }
+
+provider "aws" {
+  # Configuration options
 }
